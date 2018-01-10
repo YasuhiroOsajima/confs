5ede7b233bf93837da77b313b9a15249f3bfd8f2
YasuhiroOsajima <kimukimu33@gmail.com>
2018-01-10 05:15:59 +0000

    mod ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -2,8 +2,17 @@
 
 FILENAME=`date +%Y%m%d%H%M`.log
 LOGFILEPATH=~/logs/$FILENAME
+
 TARGETHOST=$1
+  # user@hostname
+SSHKEY=$2
+
 
 touch $LOGFILEPATH
-script -fq -c "ssh $TARGETHOST" $LOGFILEPATH
+
+if [ $SSHKEY ]; then
+    script -fq -c "ssh $TARGETHOST -i $SSHKEY" $LOGFILEPATH
+else
+    script -fq -c "ssh $TARGETHOST" $LOGFILEPATH
+fi
 
