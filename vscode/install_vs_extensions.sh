9e474607f40e89aca04392bcc273cbbddfbe5a88
Yasuhiro Osajima <kimukimu33@gmail.com>
2022-10-13 14:19:16 +0900

    update vscode config

--- vscode/install_vs_extensions.sh
+++ vscode/install_vs_extensions.sh
@@ -2,11 +2,21 @@
  
 # Visual Studio Code :: Package list
 pkglist=(
+csstools.postcss
+Dart-Code.dart-code
+Dart-Code.flutter
 DavidAnson.vscode-markdownlint
+dbaeumer.vscode-eslint
 dsznajder.es7-react-js-snippets
+eamodio.gitlens
 esbenp.prettier-vscode
-golang.go
+foxundermoon.shell-format
+hediet.vscode-drawio
+jebbs.plantuml
+mads-hartmann.bash-ide-vscode
+magicstack.MagicPython
 mechatroner.rainbow-csv
+mhutchie.git-graph
 MS-CEINTL.vscode-language-pack-ja
 ms-python.python
 ms-python.vscode-pylance
@@ -18,10 +28,19 @@ ms-toolsai.vscode-jupyter-slideshow
 ms-vscode-remote.remote-ssh
 ms-vscode-remote.remote-ssh-edit
 ms-vscode-remote.remote-wsl
+ms-vsliveshare.vsliveshare
 PKief.material-icon-theme
 rokoroku.vscode-theme-darcula
+samuelcolvin.jinjahtml
 streetsidesoftware.code-spell-checker
+styled-components.vscode-styled-components
+timonwong.shellcheck
+VisualStudioExptTeam.intellicode-api-usage-examples
+VisualStudioExptTeam.vscodeintellicode
 vscodevim.vim
+wholroyd.jinja
+yzane.markdown-pdf
+yzhang.markdown-all-in-one
 )
 
 for var in ${pkglist[@]}
